81ba66b26a56d6d20c2a0b868cd27eefee80e3f6
HaishengQWQ <139198513+HaishengQWQ@users.noreply.github.com>
2023-08-07 23:51:46 +0800

    Add files via upload

---
+++ 8.7/8.7/87.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+//#include<iostream>
+//using namespace std;
+//
+////void print(string str);
+////int max(int a, int b);
+//void printmt();
+//int main()
+//{
+//	void printmt();
+//
+//	/*int max(int a, int b)
+//	{
+//		int c;
+//		if (a < b)c = a;
+//		else c = b;
+//		return c;
+//	}
+//	void print(string str)
+//	{
+//		cout << str << endl;
+//		return;
+//	}*/
+//	return 0;
+//
+//	void printmt();
+//	{
+//		for (int ii = 1; ii <= 9; ii++)
+//		{
+//			for (int jj = 1; jj <= ii; jj++)
+//			{
+//				cout << ii << "*" << jj << "=" << ii * jj << "";
+//			}
+//			cout << endl;
+//		}
+//	}
+//}
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
