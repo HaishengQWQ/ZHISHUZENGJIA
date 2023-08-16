baf6f607b5a4ecabf4a94d8ba9ed6f20730bce83
HaishengQWQ <139198513+HaishengQWQ@users.noreply.github.com>
2023-08-16 22:47:15 +0800

    Add files via upload

---
+++ 8.16/8.16/8.16.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	//int age = 23;
+	//int height = 168;
+	//double weitht = 48.8;
+	//bool yz = false;
+
+	//cout << "result=" << (age < 25 &&height>165 && weitht < 50 && yz == true) << endl;
+	/*int a, b, c;
+	c = 10;
+	cout << "(c=10)=" << (c = 10) << endl;
+
+	a = b = 10;*/
+	/*int a, b, c, d;
+	a = 4, b = 2, c= 5;
+	d = a * (b / c);
+	cout << "d=" << d << endl;*/
+	/*bool a;
+	cout << "请输入超女颜值信息的" << endl;
+	cin >> a;
+	if (a)
+	{
+		cout << "hh" << endl;
+	}
+	else
+	{
+		cout << "nono" << endl;
+	}*/
+	/*nt arr[]{ 2,7,11,15 };
+	int targhet = 9;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < sz - 1; i++)
+	{
+		for (int j = i + 1; i < sz; i++)
+		{
+			if (arr[i] + arr[j] == targhet)
+			{
+				cout << i << j << endl;
+			}
+		}
+	}*/
+	/*cout << "苗条" << endl;*/
+	//int age = 28;
+	//int height = 170;
+	//string sc = "火辣";
+	//string yz = "漂亮";
+
+	//if (age > 25 && age < 30
+	//	&& (height > 165 && height < 175)
+	//	&&( sc == "火辣")
+	//	&& (yz == "漂亮" || yz == "一般"))
+	//{
+	//	/*if (height > 165 && height < 175)
+	//	{
+	//		if (sc == "火辣")
+	//		{
+	//			if (yz == "漂亮" || yz == "一般")
+	//			{*/
+	//				cout << "通过\n"<<endl;
+	//			//}
+	//		//}
+	//	//}
+	//}
+	/*int a = 7, b = 5, c = 0;
+	c = a > b ? a : b;
+	cout << c << endl;*/
+	/*int a =17 , b = 9 , c = 11, d;
+	d = (a > b ? a : b) > c ? (a > b ? a : b) : c;
+	cout << "d=" << d << endl;*/
+	//int sc;
+	//cin >> sc;
+	//switch (sc)
+	//{
+	//case 1:
+	//	cout << "hh";
+	//	break;
+	//case 2:
+	//	cout << "xx";
+	//	break;
+	//case 3:
+	//	cout << "yy";
+	//	break;
+	//case 4:
+	//	cout << "uu";
+	//	break;
+	//case 5:
+	//	cout << "oo";
+	//	break;
+	//default:
+	//	cout << "666";
+	//	break;
+	//}
+	/*
+	while (true)
+	{
+		cout << "请输入超女的颜值（1-漂亮，0-不漂亮）：" << endl;
+		int yz; cin >> yz;
+		if (yz == 1)
+		{
+			cout << "晋级成功捏" << endl;continue;
+		}
+		cout << "请输入超女身材（1-火辣，0-不辣";
+		int sc; cin >> sc;
+		if (sc == 1)
+		{
+			cout << "赢！" << endl;
+		}
+	}*/
+  
+}
