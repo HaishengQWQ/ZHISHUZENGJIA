b4fde9debe2abab87d04c87954d04fa001c83f91
HaishengQWQ <139198513+HaishengQWQ@users.noreply.github.com>
2023-08-16 22:55:40 +0800

    Add files via upload

---
+++ 8.14/8.14/8.14.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    /*std::cout << "姓名:" << "西施:" << "年龄:" << 25 << "体重:" << 48.5 << "性别:" << 'x';*/
+  /*  cout << "姓名:" << "西施:" << "年龄:" << 25 << "体重:" << 48.5 << "性别:" << 'x' << endl;
+   */
+   /*  string name;
+     int age;
+     double weight;
+     char sex;
+     bool yz;
+
+     name = "西施";
+     age = 25;
+     weight = 48.6;
+     sex = 'x';
+     yz = true;
+     name = "hh";
+
+     cout << "姓名" << name << "年龄" << age << weight << sex << yz << endl;*/
+     /*cout << cou << endl;
+    /* const int day = 7;
+     cout << day << endl;*/
+     /* string name;
+      cout << "请输入名字" << endl;
+      cin >> name;
+      cout << "名字是" << name << endl;
+      int age;
+      cout << "请输入年龄" << endl;
+      cin >> age;
+      cout << "年龄是" << age << endl;
+      double tz;
+      cin >> tz;
+      cout << "体重是" << tz << endl;
+      bool yz = true;
+      cout <<"hh" << yz << endl;*/
+      /*int a = 8;
+      int b = 4;
+      cout << a * b << endl;*/
+      /*   int i = 10;
+         int a;
+
+         a = i;
+         i = i + 1;
+         a = ++i;
+
+         i = i + 1;
+         a = i;
+         a = ++i;
+
+         i++;
+         cout << "i=" << i << endl;
+         cout << "a=" << a << endl;*/
+         /*int a=10;
+         int c = 20;
+
+         c += a;
+         c - +a;
+         c *= a;
+         c /= a;
+         c %= a;
+         cout << "c=" << c << endl;
+         a = c = 30;
+         cout << "a=" << c<<endl;*/
+         /*  int a = 10;
+           int b = (10);
+           int c(10);
+           cout << b << endl;
+       }*/
+    /*int a = 5;
+    int b = 8;
+    int c;
+    bool result;
+    result = a == b;
+    c = a + b;
+    cout << result << (a>b)<<endl;*/
+   /* string str1 = "西施";
+    string str2 = "西施";
+    cout << (str1 == str2) << endl;*/
+    
+}
