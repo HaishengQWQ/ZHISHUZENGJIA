4a2b46b70b911df57433fd1a655c637f41e4a9cc
HaishengQWQ <139198513+HaishengQWQ@users.noreply.github.com>
2023-08-17 22:48:35 +0800

    Add files via upload

---
+++ 8.17/8.17/8.17.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+//int main()
+//{
+//  /*  int i = 0;
+//    int j = 0;
+//    for (int i = 0; i < 4; i++)
+//    {
+//        for (int j = 0; j < 4; j++)
+//        {
+//            cout <<"这位女选手的编号是" << i << "." << j << endl;
+//        }
+//    }*/
+//    /*int i = 0;
+//    int j = 0;
+//    for (int i = 1; i<=9;i++)
+//    {
+//        for (int j = 1; j <= i; j++)
+//        {
+//            cout << i << "*" << j << "=" << i * j << " ";
+//        }
+//        cout<<endl;
+//    }*/
+//  /*  int no = 20;
+//    do
+//    {
+//        cout << no++ << endl;
+//    } while (no <= 10);*/
+//    
+//}
+//int max(int a, int b);
+////void print(string str);
+////void printmr;
+//int main()
+//{
+//	cout << "max(5,8)=" << max(5, 8) << endl;
+//}
+//int max(int a, int b)
+//{
+//
+//	if (a > b)return a;
+//	return b;
+//}
+//int aa;
+//void func1();
+//void func2();
+//int main()
+//{
+//	aa = 10;
+//	func1();
+//	func2();
+//	cout << "main aa=" << aa++ << endl;
+//}
+//void func1()
+//{
+//	cout << "func1 aa=" << aa++<<endl;
+//}
+//void func2()
+//{
+//	cout << "func2 aa=" << aa++ << endl;
+//}
+//void func(int no, string str);
+//int main()
+//{
+//	int bh = 3;
+//	string message = "zzzz";
+//}
+//void func(int no, string str)
+//{
+//	cout << "亲爱的" << no << "号" << str << endl;
+//}
