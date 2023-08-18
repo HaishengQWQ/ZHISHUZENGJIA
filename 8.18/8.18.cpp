1ae4eab49c30de2ff4e87f4dbbd721527e10c12d
HaishengQWQ <139198513+HaishengQWQ@users.noreply.github.com>
2023-08-18 23:48:59 +0800

    Add files via upload

---
+++ 8.18/8.18.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+using namespace std;
+//int main()
+//{
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//	//cout << "hh";
+//}
+//int f(int x)
+//{
+//	if (x == 0)return 0;
+//	return x + f(x - 1);
+//}
+//int main()
+//{
+//	cout << "f(100)=" << f(100) << endl;
+//}
+int main()
+{
+	//int a = sizeof(int);
+	//cout << a << endl;
+	//cout << sizeof(float) << endl;
+	//cout << sizeof(double) << endl;
+	//cout << sizeof(char) << endl;
+	//cout << sizeof(string) << endl;
+	////cout << sizeof(bool) << endl;
+	//cout << sizeof(short) << endl;
+	//cout << sizeof(unsigned short) << endl;
+	//cout << sizeof(int) << endl;
+	//cout << sizeof(unsigned int) << endl;
+	//cout << sizeof(long) << endl;
+	//cout << sizeof(unsigned long) << endl;
+	/*short i = 1000;
+	cout << "i=" << i << endl;*/
+	/*int a = 100;
+	int b = -0b110010;
+	int c = 0B100001;
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	cout << "c=" << c << endl;*/
+	/*long a = 2147483647; cout << "sizeof(a)" << sizeof(a) << ",a=" << a << endl;
+	long long b = 2147483747; cout << "sizeof(b)" << sizeof(b) << ",b=" << b << endl;*/
+	//cout << "sizeof(float)=" << sizeof(float) << endl;
+	//cout << "sizeof(doule)=" << sizeof(double) << endl;
+	//cout << "sizeof(long double)=" << sizeof(double) << endl;
+	/*float aa = 123.456789; cout << "aa=" << aa << endl;
+	double bb = 1230123456789.456789; cout << "bb=" << bb << endl;
+	long double cc = 1230123456789.456789; cout<<"cc="<<cc << endl;
+	printf("aa=%lf\n",aa);
+	printf("aa=%lf\n",bb);
+	printf("aa=%lf\n",cc);*/
+	///*char a = 88+3;*/
+	/*cout << "a=" << a<< endl;
+	cout << "sizeof(a)="<<sizeof(a) << endl;
+	if ('3' > 51)cout << "ok1\n"<<endl;
+	if ('3' == 51)cout << "ok2\n" << endl;
+	if ('3' < 50)cout << "ok3\n" << endl;*/
+	//cout << "1\t西施\n";
+	//cout << "100\t西瓜\n";
+	//cout << "10000\t 冰冰\n";
+	/*string path = "C:\\Program Files\\Microsoft OneDrive\\tail\\nation";
+	string path1 = R"(C:\Program Files\Microsoft OneDrive\tail\nation)";
+	cout << "path is" << path << endl;
+	cout << "path is" << path1 << endl;*/
+	/*string str = "西施";
+	str = "美女西施";
+	cout << "请输入超女姓名:";
+	cin >> str;
+	if (str == "冰冰")cout << "我喜欢\n";
+	
+	if (str != "冰冰")cout << "我不玩了\n";
+	str = "姓名:" + str + ",这是我现女友.";
+	cout << str << endl;*/
+	//bool a = true, b = false;
+	//cout << "sizeof(bool)=" << sizeof(bool) << endl;
+	//cout << "a=" << a << ",b=" << b << endl;
+	//cout << "a+b=" << a + b << endl;
+	/*char a = 40;
+	int b = 102400;
+	long long c = 1500000000001;
+
+	cout << "a+b+c=" << a + b + c << endl;
+	cout << "8/5=" << (double)8 / 5 << endl;
+
+	int d = 'x';
+	cout << "d=" << d << endl;*/
+	/*typedef unsigned int uint;
+	typedef	long long llong;
+	typedef unsigned long long ullong;
+
+	uint a= 1024;
+	llong b = 121313;
+	ullong c = 1234556788;
+
+	cout << "sizeof(a)="<<sizeof(uint) << ",a=" << a << endl;
+	cout << "sizeof(b)="<< sizeof(uint) << ",b=" << b << endl;
+	cout << "sizeof(c)="<< sizeof(uint) << ",c=" << c << endl;*/
+	/*typedef short int16_t;
+	typedef int int32_t;
+	typedef long long int64_t;*/
+}
+
+
